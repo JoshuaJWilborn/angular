18907d95d3e80045864ba073bbcefb0aacabdbbf
Joshua Wilborn <Joshua.J.Wilborn@gmail.com>
2013-10-09 15:17:58 -0700

    controller

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,3 +3,9 @@ var app = angular.module('myApp', [])
 app.run(function($rootScope){
   $rootScope.name = 'Josh'
 })
+
+app.controller('MyController', function($scope){
+  $scope.person = {
+    name: 'Joshua Wilborn'
+  }
+})
