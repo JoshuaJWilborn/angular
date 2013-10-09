1de4714708ec9d753564a30f31d33e963001426b
Joshua Wilborn <Joshua.J.Wilborn@gmail.com>
2013-10-09 15:00:39 -0700

    Hello World

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angular::Application.config.secret_key_base = '1b3968334a880af2cb7cbe6384478ad96391745bd0f3a6cb8df68025c1650fd8efc4f15e17ef7654939689b20888dadc91360f6e2643069ec8def4c149676e6e'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Angular::Application.routes.draw do
+  root to: 'public/index.html'
+end
